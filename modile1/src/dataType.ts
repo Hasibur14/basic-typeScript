fad3c8cadf268b9f38886b6c629908320baadcd4
hasibur14 <hasib141415@gmail.com>
2024-11-01 23:55:36 +0600

    add array data type

---
+++ modile1/src/dataType.ts
@@ -0,0 +1,20 @@
+//  string
+let country : string = 'Bangladesh'
+
+// Number
+let myRoll : number = 15545744
+
+// boolean
+let isAdmin : boolean = true
+
+// undefine
+let a:undefined =undefined
+
+// null
+let b :null =null
+
+
+// Array
+let friend:string[] = ['hasib', 'sakib'];
+
+let eligibleRollList :[number, string, boolean] = [1561, 'hasib', true]
